b8f024ab8d5f13cc790cb9afd5418d4d0b0154db
atajwar22 <atajwar22@andover.edu>
2020-07-31 11:39:16 -0400

    myWebsite

---
+++ script.js
@@ -0,0 +1,14 @@
+//let guitar = document.querySelector("#guitarPic");
+//let hoverText = document.createTextNode("Yes, I know my hair was terrible, and no, that is not my dad.");
+
+//function mouseOver() {
+  // guitar.appendChild(hoverText);
+   //guitar.style.color = "black";
+   //guitar.style.textAlign = "center";
+//}
+//function mouseout() {
+//    guitar.style.color = "transparent";
+//}
+
+//guitar.onmouseover = function() {mouseOver()};
+//guitar.onmouseout = function() {mouseout()};
