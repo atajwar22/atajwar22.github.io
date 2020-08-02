a5aa4d733a45a92f8d3e7217f198bdeb71078e9b
atajwar22 <atajwar22@andover.edu>
2020-08-01 21:47:37 -0400

    myWebsite

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-//let guitar = document.querySelector("#guitarPic");
-//let hoverText = document.createTextNode("Yes, I know my hair was terrible, and no, that is not my dad.");
+let guitarPic = document.querySelector("#guitarPic")
+let guitar = document.querySelector("#guitar");
+let hoverText = document.createTextNode("Yes, I know my hair was terrible, and NO, that is not my dad!");
 
-//function mouseOver() {
-  // guitar.appendChild(hoverText);
-   //guitar.style.color = "black";
-   //guitar.style.textAlign = "center";
-//}
-//function mouseout() {
-//    guitar.style.color = "transparent";
-//}
+function onPic() {
+   guitar.appendChild(hoverText);
+   guitar.style.color = "#F2B880";
+   guitar.style.textAlign = "center";
+   guitar.style.display = "inline-block";
+}
+function outPic() {
+    guitar.style.display = "none";
+}
 
-//guitar.onmouseover = function() {mouseOver()};
-//guitar.onmouseout = function() {mouseout()};
+guitarPic.addEventListener("mouseover", onPic);
+guitarPic.addEventListener("mouseout", outPic);
 
 //BUTTON HOVER EFFECT
 let button = document.querySelector("button");
 function mouseOver() {
-  button.style.backgroundColor = "white";
+  button.style.backgroundColor = "#DCE0D9";
   button.style.borderColor = "#FFF9A5";
 }
 function mouseOut() {
-  button.style.backgroundColor = "#DCE0D9";
+  button.style.backgroundColor = "#white";
   button.style.borderColor = "black";
 }
 button.addEventListener("mouseover", mouseOver);
