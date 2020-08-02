3d0cbd9df724305556ae6522bdac1b3756e97212
atajwar22 <atajwar22@andover.edu>
2020-08-01 20:33:47 -0400

    myWebsite

--- script.js
+++ script.js
@@ -12,3 +12,16 @@
 
 //guitar.onmouseover = function() {mouseOver()};
 //guitar.onmouseout = function() {mouseout()};
+
+//BUTTON HOVER EFFECT
+let button = document.querySelector("button");
+function mouseOver() {
+  button.style.backgroundColor = "white";
+  button.style.borderColor = "#FFF9A5";
+}
+function mouseOut() {
+  button.style.backgroundColor = "#DCE0D9";
+  button.style.borderColor = "black";
+}
+button.addEventListener("mouseover", mouseOver);
+button.addEventListener("mouseout", mouseOut);
